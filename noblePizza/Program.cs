c318461b9efa3e327e475ea8af511c7a71edff3c
Eliseo <Eliseo@LAPTOP-OIA8MLF7>
2023-10-23 08:34:01 +0700

    Add project files.

---
+++ noblePizza/Program.cs
@@ -0,0 +1,18 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        const double PI = 3.14;
+        const double SLICE_AREA = 14.13;
+
+        double radius;
+        int numberOfSlices;
+
+        Console.WriteLine("Enter the Radius of the Pizza");
+        radius = double.Parse(Console.ReadLine());
+
+        numberOfSlices = (int)((PI * radius * radius) / SLICE_AREA);
+
+        Console.WriteLine($"{numberOfSlices} is the total slices of the Pizza");
+    }
+}
